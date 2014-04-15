57f556b375c0c580ef2bdcf2216bbc11aaabffe8
Yasuharu Sawada <sawada_yasuharu@cyberagent.co.jp>
2014-04-15 14:09:38 +0900

    マーカーを削除

--- sawada_yasuharu/MyRPS.java
+++ sawada_yasuharu/MyRPS.java
@@ -18,7 +18,7 @@ public class MyRPS {
      * @param num 入力されたint
      * @return 手を示す列挙型
      */
-    public Hand parseFromInt(int num){//{{{
+    public Hand parseFromInt(int num){
         switch (num) {
             case 1:
                 return Hand.Rock;
@@ -28,14 +28,14 @@ public class MyRPS {
                 return Hand.Sissors;
         }
         return null;
-    }//}}}
+    }
     
     /**
      * 列挙型を文字列に変換
      * @param hand 手を示す列挙型
      * @return 手を示す文字列
      */
-    public String convertToString(Hand hand){//{{{
+    public String convertToString(Hand hand){
         switch (hand) {
             case Rock:
                 return "グー";
@@ -45,20 +45,20 @@ public class MyRPS {
                 return "チョキ";
         }
         return null;
-    }//}}}
+    }
 
     /**
      * 入力された数字に不正がないか確かめる
      * @param inputNum 入力された数字
      * @return 入力に不正がないかを示すboolean
      */
-    public boolean isValidInput(int inputNum){//{{{
+    public boolean isValidInput(int inputNum){
         if(inputNum < 1 || inputNum >= 4){
             return false;
         }else{
             return true;
         }
-    }//}}}
+    }
 
     /**
      * 勝敗を示す列挙型を返す
@@ -66,7 +66,7 @@ public class MyRPS {
      * @param enemyHand コンピュータの手を表す列挙型
      * @return 勝敗を示す列挙型を返す
      */
-    public Result play(Hand ownHand, Hand enemyHand){//{{{
+    public Result play(Hand ownHand, Hand enemyHand){
         if(ownHand == enemyHand){
             return Result.Draw;
         }else{
@@ -79,13 +79,13 @@ public class MyRPS {
                 return Result.Youlose;
             }
         }
-    }//}}}
+    }
 
     /**
      * じゃんけんを繰り返す部分
      * ユーザに入力を求める際には、不正な入力がされるまで入力を繰り返し求める
      */
-    public void doGame(){//{{{
+    public void doGame(){
         boolean firstTime = true;
         int ownNum = 0;
         int enemyNum = 0;
@@ -94,7 +94,7 @@ public class MyRPS {
         Result result;
 
         do{
-            // 適切な入力があるまで繰り返し //{{{
+            // 適切な入力があるまで繰り返し 
             do{
                 System.out.print("出す手を入力 => ");
                 BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -103,7 +103,7 @@ public class MyRPS {
                 }catch(IOException e){
                     System.out.println(e);
                 }
-            }while(!isValidInput(ownNum));//}}}
+            }while(!isValidInput(ownNum));
 
             // 最初はぽん！
             if(firstTime){
@@ -134,7 +134,7 @@ public class MyRPS {
         }else{
             System.out.println("あなたの負けです！");
         }
-    }//}}}
+    }
 
     public static void main(String[] args) {
         System.out.println("じゃんけんをしましょう!");
