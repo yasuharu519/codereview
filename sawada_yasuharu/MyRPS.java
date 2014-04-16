fbbe661dbead567f63ada8636d2a9192cb6ef91b
Yasuharu Sawada <sawada_yasuharu@cyberagent.co.jp>
2014-04-16 15:45:34 +0900

    コードレビューをうけて修正

--- sawada_yasuharu/MyRPS.java
+++ sawada_yasuharu/MyRPS.java
@@ -2,49 +2,66 @@ import java.io.*;
 
 public class MyRPS {
     enum Hand {
-        Rock,
-        Paper,
-        Sissors
+        Rock("グー"),
+        Paper("パー"),
+        Sissors("チョキ");
+
+        private String name;
+
+        private Hand(String _name){
+            this.name = _name;
+        }
+
+        public static Hand parseFromInt(int num){
+            switch (num) {
+                case 1:
+                    return Hand.Rock;
+                case 2:
+                    return Hand.Paper;
+                case 3:
+                    return Hand.Sissors;
+            }
+            return null;
+        }
+
+        public Result playRPS(Hand other){
+            if(this == other){
+                return Result.Draw;
+            }else{
+                if( (this == Hand.Rock && other == Hand.Sissors) ||
+                    (this == Hand.Paper && other == Hand.Rock) ||
+                    (this == Hand.Sissors && other == Hand.Paper)){
+                    return Result.YouWon;
+                }else{
+                    return Result.YouLost;
+                }
+            }
+        }
+
+        @Override
+        public String toString(){
+            return this.name;
+        }
     }
 
     enum Result{
-        Youwon,
-        Youlose,
-        Draw
-    }
+        YouWon,
+        YouLost,
+        Draw;
 
-    /**
-     * 入力されたintを手を示す列挙型に変換
-     * @param num 入力されたint
-     * @return 手を示す列挙型
-     */
-    public Hand parseFromInt(int num){
-        switch (num) {
-            case 1:
-                return Hand.Rock;
-            case 2:
-                return Hand.Paper;
-            case 3:
-                return Hand.Sissors;
+        public void printResult(){
+            switch (this){
+                case YouWon:
+                    System.out.println("あなたの勝ちです");
+                    break;
+                case YouLost:
+                    System.out.println("あなたの負けです");
+                    break;
+                default:
+                    System.out.println("引き分けです");
+                    break;
+            }
         }
-        return null;
-    }
-    
-    /**
-     * 列挙型を文字列に変換
-     * @param hand 手を示す列挙型
-     * @return 手を示す文字列
-     */
-    public String convertToString(Hand hand){
-        switch (hand) {
-            case Rock:
-                return "グー";
-            case Paper:
-                return "パー";
-            case Sissors:
-                return "チョキ";
-        }
-        return null;
     }
 
     /**
@@ -53,32 +70,7 @@ public class MyRPS {
      * @return 入力に不正がないかを示すboolean
      */
     public boolean isValidInput(int inputNum){
-        if(inputNum < 1 || inputNum >= 4){
-            return false;
-        }else{
-            return true;
-        }
-    }
-
-    /**
-     * 勝敗を示す列挙型を返す
-     * @param ownHand ユーザの手を表す列挙型
-     * @param enemyHand コンピュータの手を表す列挙型
-     * @return 勝敗を示す列挙型を返す
-     */
-    public Result play(Hand ownHand, Hand enemyHand){
-        if(ownHand == enemyHand){
-            return Result.Draw;
-        }else{
-            if( (ownHand == Hand.Rock && enemyHand == Hand.Sissors) || 
-                (ownHand == Hand.Paper && enemyHand == Hand.Rock) ||
-                (ownHand == Hand.Sissors && enemyHand == Hand.Paper)){
-                return Result.Youwon;
-            }
-            else{
-                return Result.Youlose;
-            }
-        }
+        return (inputNum >= 1 && inputNum < 4);
     }
 
     /**
@@ -86,16 +78,12 @@ public class MyRPS {
      * ユーザに入力を求める際には、不正な入力がされるまで入力を繰り返し求める
      */
     public void doGame(){
-        boolean firstTime = true;
-        int ownNum = 0;
-        int enemyNum = 0;
-        Hand ownHand;
-        Hand enemyHand;
-        Result result;
+        Result result = null;
 
         do{
             // 適切な入力があるまで繰り返し 
-            do{
+            int ownNum = 0;
+            while(!isValidInput(ownNum)){
                 System.out.print("出す手を入力 => ");
                 BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
                 try {
@@ -103,37 +91,30 @@ public class MyRPS {
                 }catch(IOException e){
                     System.out.println(e);
                 }
-            }while(!isValidInput(ownNum));
+            }
 
-            // 最初はぽん！
-            if(firstTime){
+            if(result != Result.Draw){
                 System.out.println("ぽん！");
-                firstTime = false;
             }else{
                 System.out.println("しょ！");
             }
 
             // 敵の出す手を計算
-            enemyNum = (int)(Math.random() * 3) + 1;
+            int enemyNum = (int)(Math.random() * 3) + 1;
             // 列挙型に変換
-            ownHand = parseFromInt(ownNum);
-            enemyHand = parseFromInt(enemyNum);
+            Hand ownHand = Hand.parseFromInt(ownNum);
+            Hand enemyHand = Hand.parseFromInt(enemyNum);
             // 出した手を出力
-            System.out.printf("あなた: %s、コンピュータ: %s\n",
-                    convertToString(ownHand), convertToString(enemyHand));
+            System.out.printf("あなた: %s、コンピュータ: %s\n", ownHand, enemyHand);
             // 結果
-            result = play(ownHand, enemyHand);
+            result = ownHand.playRPS(enemyHand);
             if(result == Result.Draw){
                 System.out.println("あーいこーで・・");
             }
         }while(result == Result.Draw);
 
         // 最終的な結果の出力
-        if(result == Result.Youwon){
-            System.out.println("あなたの勝ちです！");
-        }else{
-            System.out.println("あなたの負けです！");
-        }
+        result.printResult();
     }
 
     public static void main(String[] args) {
