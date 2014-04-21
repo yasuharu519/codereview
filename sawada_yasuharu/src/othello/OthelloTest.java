aeb3dc501125854c86e8ed866c3b292a66480502
Yasuharu Sawada <sawada_yasuharu@cyberagent.co.jp>
2014-04-21 18:54:34 +0900

    余計なテストケースが混じっていたのを削除

--- sawada_yasuharu/src/othello/OthelloTest.java
+++ sawada_yasuharu/src/othello/OthelloTest.java
@@ -20,51 +20,7 @@ public class OthelloTest {
 
     @Test
     public void 最初手札が5枚配られる() {//{{{
-        othello othello = new othello();
 
-        assertThat(othello.getHand().size(), is(5));
     }//}}}
 
-    @Test
-    public void ゼロを入力すると手札を交換しない() {//{{{
-        othello othello = new othello();
-        
-        List<Card> hand = othello.getHand();
-        int[] exchangeNums = {0};
-
-        othello.exchangeCards(exchangeNums);
-
-        assertThat(hand, is(othello.getHand()));
-    }//}}}
-
-    @Test
-    public void 手札は交換できる_1枚() {//{{{
-        othello othello = new othello();
-
-        Card oldCard = othello.getHand().get(0);
-        int[] exchangeNums = {1};
-
-        othello.exchangeCards(exchangeNums);
-        
-        assertThat(othello.getHand().get(0), is(not(oldCard)));
-    }//}}}
-
-    @Test
-    public void 手札は交換できる_5枚全部() {//{{{
-        othello othello = new othello();
-
-        List<Card> oldCards = new ArrayList<Card>();
-        for (Card c : othello.getHand()) {
-            oldCards.add(c);
-        }
-
-        int[] exchangeNums = {1, 2, 3, 4, 5};
-
-        othello.exchangeCards(exchangeNums);
-        List<Card> newCards = othello.getHand();
-        
-        for (int i = 0; i < oldCards.size(); i++) {
-            assertThat(oldCards.get(i), is(not(newCards.get(i))));
-        }
-    }//}}}
 }
