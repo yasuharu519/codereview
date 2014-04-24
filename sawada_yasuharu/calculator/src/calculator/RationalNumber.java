280df8962cfc9bda56a51d7faf69c5487dd56cdd
Yasuharu Sawada <sawada_yasuharu@cyberagent.co.jp>
2014-04-24 16:48:53 +0900

    RationalNumberのjavadocの追加

--- sawada_yasuharu/calculator/src/calculator/RationalNumber.java
+++ sawada_yasuharu/calculator/src/calculator/RationalNumber.java
@@ -1,20 +1,34 @@
 package calculator;
 
+/**
+ * 有理数を表現するクラス.
+ */
 public class RationalNumber{
     public int numerator;
     public int denominator;
 
+    /**
+     * intのみを入力として受け付けるコンストラクタ.
+     */
     public RationalNumber(int number){
         this.numerator = number;
         this.denominator = 1;
     }
 
+    /**
+     * 分子、分母を入力として受け取るコンストラクタ.
+     * 入力後約分を行う.
+     */
     public RationalNumber(int numerator, int denominator){
         this.numerator = numerator;
         this.denominator = denominator;
         reduction();
     }
 
+    /**
+     * 少数を入力として受け取るコンストラクタ.
+     * 有理数に変換を行う
+     */
     public RationalNumber(double number){
         int count = 0;
         while(!isInteger(number)){
@@ -29,6 +43,9 @@ public class RationalNumber{
     // Public methods
     ///////////////////////////////////////////////////////////////////////////
 
+    /**
+     * 有理数同士の足し算.
+     */
     public RationalNumber plus(RationalNumber other){
         // 通分
         if(this.denominator != other.denominator){
@@ -42,6 +59,9 @@ public class RationalNumber{
         return this;
     }
 
+    /**
+     * 有理数同士の引き算.
+     */
     public RationalNumber minus(RationalNumber other){
         // 通分
         if(this.denominator != other.denominator){
@@ -56,6 +76,9 @@ public class RationalNumber{
         return this;
     }
 
+    /**
+     * 有理数同士の掛け算.
+     */
     public RationalNumber multiply(RationalNumber other){
         this.numerator *= other.numerator;
         this.denominator *= other.denominator;
@@ -64,6 +87,9 @@ public class RationalNumber{
         return this;
     }
 
+    /**
+     * 有理数同士の割り算.
+     */
     public RationalNumber divide(RationalNumber other){
         this.numerator *= other.denominator;
         this.denominator *= other.numerator;
@@ -72,6 +98,11 @@ public class RationalNumber{
         return this;
     }
 
+    /**
+     * 有理数のString表現を行う.
+     * trueを受け取ると少数で表現を行い、
+     * falseを受け取ると分数で表現する
+     */
     public String toString(boolean printInDouble){
         if (printInDouble) {
             return String.format("%f",
@@ -106,12 +137,19 @@ public class RationalNumber{
 
     // Private methods
     ///////////////////////////////////////////////////////////////////////////
+    
+    /**
+     * 約分を行う関数.
+     */
     private void reduction(){
         int gcd = gcd(numerator, denominator);
         this.numerator /= gcd;
         this.denominator /= gcd;
     }
 
+    /**
+     * 最大公約数を求める関数.
+     */
     private int gcd(int m, int n){
         if (n == 0) {
             return m;
@@ -120,6 +158,9 @@ public class RationalNumber{
         }
     }
 
+    /**
+     * doubleで入力された値がintにキャストした値と等しいかチェックする関数.
+     */
     private boolean isInteger(double value){
         return ((value == Math.floor(value)) && !Double.isInfinite(value));
     }
